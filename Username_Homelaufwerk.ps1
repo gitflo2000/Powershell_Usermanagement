81c686ad7c57cc9dc2be067639087bc87fb9e985
gitflo2000 <36086323+gitflo2000@users.noreply.github.com>
2026-09-10 17:04:57 +0200

    Add files via upload

---
+++ Username_Homelaufwerk.ps1
@@ -0,0 +1,4 @@
+﻿#Write-Host "Username: $env:UserName"
+$cred = Get-Credential -Credential Contoso\ServiceAccount
+New-PSDrive -Name "H" -Root "\\bs19hh.de\Unterricht\Lehrerdaten\$env:UserName" -Persist -PSProvider "FileSystem" -Credential $cred
+Net Use
